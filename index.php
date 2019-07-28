ba99be546ec33cbf25a9ab831587bf923c429efd
gabriel096 <gabriellucaslima96@gmai.com>
2019-07-28 06:58:21 -0300

    classe page tamplete

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once("vendor/autoload.php");
+	
+	use \slim\Slim;
+	use \Hcode\Page; 
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$page = new Page(); 
+
+	$page->setTpl("index"); 
+
+});
+
+$app->run();
+
+ ?>
